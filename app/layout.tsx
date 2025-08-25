1ceb1dc013e38ef0e52a0baeadcb59f413bb6a22
anglitj <jemanglit@gmail.com>
2025-08-25 21:29:40 +0800

    Remove SessionWrapper from RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import SessionWrapper from "@/components/session-wrapper";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SessionWrapper>{children}</SessionWrapper>
+        {children}
       </body>
     </html>
   );
